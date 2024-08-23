0336cc9852100c18320039309d55581b2aae1106
Abdallah008 <abdallahjamshaid@yahoo.com>
2024-08-22 19:21:23 -0700

    Completly modified

--- js/script.js
+++ js/script.js
@@ -1,35 +1,58 @@
-function addContact() {
+function myFunction() {
+  let userChoice;
+  let contacts = [];
 
-    let userChoice;
-    let contacts = [];
+  do {
+    userChoice = prompt("Choose an option: first, last, new, or quit");
+    if (userChoice === "first") {
+      console.log(
+        "First contact: " +
+          contacts[0].name +
+          " / " +
+          contacts[0].phone +
+          " / " +
+          contacts[0].email
+      );
+    } else if (userChoice === "last") {
+      console.log(
+        "Last contact: " +
+          contacts[contacts.length - 1].name +
+          " / " +
+          contacts[contacts.length - 1].phone +
+          " / " +
+          contacts[contacts.length - 1].email
+      );
+    } else if (userChoice === "new") {
+      let newName = prompt("Enter name:");
+      let newPhone = prompt("Enter phone number:");
+      let newEmail = prompt("Enter email address:");
 
-    do {
-        userChoice = prompt("Choose an option: first, last, new, or quit");
-        if (userChoice === "first") {
-            console.log("First contact: " + contacts[0].name + " / " + contacts[0].phone + " / " + contacts[0].email);
-        } else if (userChoice === "last") {
-            console.log("Last contact: " + contacts[contacts.length - 1].name + " / " + contacts[contacts.length - 1].phone + " / " + contacts[contacts.length - 1].email);
-        } else if (userChoice === "new") {
-            let newName = prompt("Enter name:");
-            let newPhone = prompt("Enter phone number:");
-            let newEmail = prompt("Enter email address:");
+      // Validate input
+      if (!newName || isNaN(newPhone) || !newEmail.includes("@")) {
+        alert("Invalid input. Please try again.");
+        continue;
+      }
 
-            // Validate input
-            if (!newName || isNaN(newPhone) || !newEmail.includes("@")) {
-                alert("Invalid input. Please try again.");
-                continue;
-            }
-
-            // Add new contact
-            contacts.push({
-                name: newName,
-                phone: newPhone,
-                email: newEmail
-            });
-            console.log("New contact added: " + newName + " " + newPhone);
-        } else if (userChoice === "quit") {
-            break;
-        }
-    } while (true);
-
+      // Add new contact
+      contacts.push({
+        name: newName,
+        phone: newPhone,
+        email: newEmail,
+      });
+      let contact_list = document.getElementById("contact_list");
+      for (let i = 0; i < contacts.length; i++) {
+        contact_list.innerHTML +=
+          "<li class='list-group-item'>" +
+          contacts[0]["name"] +
+          " / " +
+          contacts[0]["phone"] +
+          " / " +
+          contacts[0]["email"] +
+          "</li>";
+      }
+      console.log("New contact added: " + newName + " " + newPhone);
+    } else if (userChoice === "quit") {
+      break;
+    }
+  } while (true);
 }
